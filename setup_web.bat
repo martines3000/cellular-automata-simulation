9442da4d7773e9eee2d9ad4fd78f191734b6f2b0
Martin Domajnko <domajnko.martin@gmail.com>
2022-03-26 13:02:00 +0100

    Changes docker build

--- setup_web.bat
+++ setup_web.bat
@@ -2,6 +2,3 @@
 rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 cargo update -p wasm-bindgen
-
-@REM For local tests with `./start_server`:
-cargo install basic-http-server
--- setup_web.sh
+++ setup_web.sh
@@ -5,6 +5,3 @@ set -eu
 rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 cargo update -p wasm-bindgen
-
-# For local tests with `./start_server`:
-cargo install basic-http-server
